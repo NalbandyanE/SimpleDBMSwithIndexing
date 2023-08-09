fbdfba8a538ee6e2c69e705c92a378031217be27
Emi <89019222+NalbandyanE@users.noreply.github.com>
2023-08-09 18:30:43 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,14 +6,6 @@
 int main() {
 	std::cout << "Welcome to Simple DBMS." << std::endl;
 	std::cout << "Enter your SQL command (or 'exit' to quit): \n";
-    // std::string input;
-    // std::getline(std::cin, input);
-    // Command com(input);
-    // std::cout << com.getName() << "\n";
-    // for (int i = 0; i < com.getArguments().size(); ++i) {
-    //     std::cout << com.getArguments()[i] << " ";
-    // }
-    // std::cout << std::endl;
 	CommandExecutor executor;
 	while (true) {
 		std::string input;
